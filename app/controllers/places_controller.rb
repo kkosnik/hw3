7edf807fd74935b8807a796979d8140cc23d6d72
kkosnik <kelly.kosnik@kellogg.northwestern.edu>
2022-02-18 21:11:42 +0000

    got places form to work

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,4 +4,19 @@ class PlacesController < ApplicationController
         @places = Place.all
       end
 
+      def show
+        # .find(123) is the same as .where({ id: 123 })[0]
+        @place = Place.find(params["id"])
+      end
+    
+      def new
+        @place = Place.new
+      end  
+    
+      def create
+        @place = Place.new(params["place"])
+        @place.save
+        redirect_to "/places"
+      end
+
 end
